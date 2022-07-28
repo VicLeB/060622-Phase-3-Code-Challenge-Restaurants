5700dfe8ce13340cfc01be70627465f665287e76
Victoria LeBel <vickileb@hotmail.com>
2022-07-28 10:22:58 -0700

    commit tree ad5f13b5e85e10e10218f1e24cef308603837c23

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,23 @@
 class Customer < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :restaurants, through: :reviews
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def favorite_restaurant
+        self.reviews.all.order("star_rating").last.restaurant
+
+    end
+
+    def add_review(restaurant, rating)
+        Review.create(star_rating: rating, restaurant: restaurant, customer: self)
+    end
+
+    def delete_reviews(restaurant)
+        reviews = Review.where(restaurant_id: restaurant.id, customer_id: self.id)
+        reviews.destroy_all
+    end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,13 @@
 class Restaurant < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :customers, through: :reviews
+
+    def self.fanciest
+        Restaurant.all.order("price").last
+    end
+
+    def all_reviews
+        reviews.map{|r| r.full_review}
+    end
+
 end
